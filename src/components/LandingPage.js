0d2ef0f036aafa9d8b8bcfe91b6ff516e6beaecd
Pranav Srivastav <109237040+iamransh@users.noreply.github.com>
2023-08-31 05:10:28 +0530

    Live Update

---
+++ src/components/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LandingPage() {
+  return (
+    <div className='box'>
+        <h1 className='heading'>Sui-In-Style</h1>
+        <h2 className='cont'>{"We'll Be Live Soon!!"}</h2>
+    </div>
+  )
+}
