f4cbe8faba059d6acbbe9264792050494b96ffd4
Pranav Srivastav <109237040+iamransh@users.noreply.github.com>
2023-09-03 22:04:26 +0530

    phone font size change

--- src/components/About.js
+++ src/components/About.js
@@ -110,7 +110,7 @@ export default function About() {
               <Text fontSize={["30px", "28px", "25px"]}>
                 My name is Sangeeta Srivastava
               </Text>
-              <Text fontSize={["25px", "23px", "20px"]}>
+              <Text fontSize={["22px", "20px", "17px"]}>
                 From a bored housewife, now thriving as a successful business
                 owner, crafting dreams for women with passion and precision.
               </Text>
